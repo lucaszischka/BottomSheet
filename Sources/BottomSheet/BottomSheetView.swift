417e92fb76385492149b15e21615255d593e5f0b
Lucas Zischka <63545066+LucasMucGH@users.noreply.github.com>
2021-03-05 08:46:12 +0100

    Added flick through feature

--- Sources/BottomSheet/BottomSheetView.swift
+++ Sources/BottomSheet/BottomSheetView.swift
@@ -61,7 +61,15 @@ fileprivate struct BottomSheetView<hContent: View, mContent: View>: View {
                             }
                             .onEnded { value in
                                 if resizeable {
-                                    if abs(self.translation) > geometry.size.height * 0.1 {
+                                    if abs(value.translation.height) > geometry.size.height * 0.3 {
+                                        if value.translation.height < 0 {
+                                            self.switchPositionUp()
+                                            self.switchPositionUp()
+                                        } else if value.translation.height > 0 {
+                                            self.switchPositionDown()
+                                            self.switchPositionDown()
+                                        }
+                                    } else if abs(value.translation.height) > geometry.size.height * 0.1 {
                                         if value.translation.height < 0 {
                                             self.switchPositionUp()
                                         } else if value.translation.height > 0 {
@@ -100,7 +108,15 @@ fileprivate struct BottomSheetView<hContent: View, mContent: View>: View {
                             }
                             .onEnded { value in
                                 if resizeable {
-                                    if abs(self.translation) > geometry.size.height * 0.1 {
+                                    if abs(value.translation.height) > geometry.size.height * 0.3 {
+                                        if value.translation.height < 0 {
+                                            self.switchPositionUp()
+                                            self.switchPositionUp()
+                                        } else if value.translation.height > 0 {
+                                            self.switchPositionDown()
+                                            self.switchPositionDown()
+                                        }
+                                    } else if abs(value.translation.height) > geometry.size.height * 0.1 {
                                         if value.translation.height < 0 {
                                             self.switchPositionUp()
                                         } else if value.translation.height > 0 {
