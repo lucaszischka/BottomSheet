87a33d608bcd2dbf951f19878078c08e4ff323b4
Lucas Zischka <63545066+LucasMucGH@users.noreply.github.com>
2021-12-28 21:00:17 +0100

    try divide

--- Sources/BottomSheet/BottomSheetView.swift
+++ Sources/BottomSheet/BottomSheetView.swift
@@ -113,9 +113,9 @@ internal struct BottomSheetView<hContent: View, mContent: View, bottomSheetPosit
                                             if (self.isTopPosition && offset.y > 0) {
                                                 self.isScrollEnabled = true
                                                 
-                                                print(offset.y, offset.y * offset.y * (offset.y / 100))
+                                                print(offset.y, offset.y * offset.y / 100)
                                                 
-                                                self.translation = offset.y * offset.y * (offset.y / 100)
+                                                self.translation = offset.y * offset.y / 100
                                                 self.endEditing()
                                                 
                                                 let height: CGFloat = self.translation / geometry.size.height
