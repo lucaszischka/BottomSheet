a9209ff857b18ce02a01f2fa31652db7eecc0bef
Lucas Zischka <63545066+LucasMucGH@users.noreply.github.com>
2021-12-28 20:46:59 +0100

    drop calculation

--- Sources/BottomSheet/BottomSheetView.swift
+++ Sources/BottomSheet/BottomSheetView.swift
@@ -113,10 +113,10 @@ internal struct BottomSheetView<hContent: View, mContent: View, bottomSheetPosit
                                             if (self.isTopPosition && offset.y > 0) {
                                                 self.isScrollEnabled = true
                                                 
-                                                self.translation = offset.y * offset.y * (offset.y * 0.1)
+                                                self.translation += offset.y
                                                 self.endEditing()
                                                 
-                                                let height: CGFloat = (offset.y * offset.y * (offset.y * 0.1)) / geometry.size.height
+                                                let height: CGFloat = self.translation / geometry.size.height
                                                 self.switchPosition(with: height)
                                             } else if !self.isTopPosition {
                                                 self.isScrollEnabled = false
