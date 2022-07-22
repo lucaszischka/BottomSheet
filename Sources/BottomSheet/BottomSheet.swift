429afc609f5d3ddba84e46cd5e09706448167c5c
Lucas Zischka <63545066+lucaszischka@users.noreply.github.com>
2022-07-22 20:18:54 +0200

    mhmmm

--- Sources/BottomSheet/BottomSheet.swift
+++ Sources/BottomSheet/BottomSheet.swift
@@ -38,6 +38,10 @@ public struct BottomSheet<HContent: View, MContent: View, V: View>: View {
                 )
                 // zIndex ensures that the transition is visible on disappear
                     .zIndex(1)
+                    .frame(
+                        maxWidth: .infinity,
+                        maxHeight: .infinity
+                    )
             }
         }
     }
