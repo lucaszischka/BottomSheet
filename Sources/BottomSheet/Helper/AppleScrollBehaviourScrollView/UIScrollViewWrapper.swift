cf27194d0177a8f4fc2bac110d9d66b79919d1d6
Lucas Zischka <63545066+lucaszischka@users.noreply.github.com>
2022-07-28 18:05:31 +0200

    Readd it

--- Sources/BottomSheet/Helper/AppleScrollBehaviourScrollView/UIScrollViewWrapper.swift
+++ Sources/BottomSheet/Helper/AppleScrollBehaviourScrollView/UIScrollViewWrapper.swift
@@ -41,16 +41,19 @@ internal struct UIScrollViewWrapper<Content: View>: UIViewControllerRepresentabl
         }
         viewController.scrollView.contentSize = contentSize
         
-        viewController.scrollView.contentInsetAdjustmentBehavior = .never
+//        viewController.scrollView.contentInsetAdjustmentBehavior = .never
         
         // Align content view
-//        NSLayoutConstraint.activate([
-//            viewController.hostingController.view.leadingAnchor.constraint(equalTo: viewController.scrollView.leadingAnchor),
-//            viewController.hostingController.view.trailingAnchor.constraint(equalTo: viewController.scrollView.trailingAnchor),
-//            viewController.hostingController.view.topAnchor.constraint(equalTo: viewController.scrollView.topAnchor),
-//            viewController.hostingController.view.bottomAnchor.constraint(equalTo: viewController.scrollView.bottomAnchor)
-//        ])
-//        viewController.view.setNeedsUpdateConstraints()
+        NSLayoutConstraint.activate([
+            viewController.hostingController.view.leadingAnchor.constraint(equalTo: viewController.scrollView.leadingAnchor),
+            viewController.hostingController.view.trailingAnchor.constraint(equalTo: viewController.scrollView.trailingAnchor),
+            viewController.hostingController.view.topAnchor.constraint(equalTo: viewController.scrollView.topAnchor)
+        ])
+        let bottomAnchor = viewController.hostingController.view.bottomAnchor.constraint(equalTo: viewController.scrollView.bottomAnchor)
+        bottomAnchor.priority = .defaultLow
+        bottomAnchor.isActive = true
+        
+        viewController.view.setNeedsUpdateConstraints()
         viewController.view.updateConstraintsIfNeeded()
         viewController.view.layoutIfNeeded()
         
