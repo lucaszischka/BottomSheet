0e7c2b317c6dd8dbc4dd7f512ab3b6d62f91f4ff
Lucas Zischka <63545066+lucaszischka@users.noreply.github.com>
2022-07-28 22:47:45 +0200

    Remove safe area?

--- Sources/BottomSheet/Helper/AppleScrollBehaviourScrollView/UIScrollViewWrapper.swift
+++ Sources/BottomSheet/Helper/AppleScrollBehaviourScrollView/UIScrollViewWrapper.swift
@@ -34,15 +34,15 @@ internal struct UIScrollViewWrapper<Content: View>: UIViewControllerRepresentabl
         var contentSize: CGSize = viewController.hostingController.view.intrinsicContentSize
         contentSize.width = viewController.scrollView.frame.width
         viewController.hostingController.view.frame.size = contentSize
-        if contentSize.height < viewController.scrollView.frame.height {
+        if contentSize.height <= viewController.scrollView.frame.height {
             contentSize.height = viewController.scrollView.frame.height
             viewController.scrollView.alwaysBounceVertical = true
         }
         viewController.scrollView.contentSize = contentSize
         
         viewController.scrollView.contentInsetAdjustmentBehavior = .never
-//        viewController.scrollView.contentInset = .zero
-//        viewController.hostingController.view.safeAreaInsets = .zero
+        viewController.scrollView.contentInset = .zero
+        viewController.hostingController.view.safeAreaInsets = .zero
         
         // Layout the UIHostingController
         NSLayoutConstraint.activate([
