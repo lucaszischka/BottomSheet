9d984fe5c7a62e42fc19f94450f6582250f7fd5f
Lucas Zischka <63545066+lucaszischka@users.noreply.github.com>
2022-07-28 17:34:32 +0200

    Maybe this was the issue

--- Sources/BottomSheet/Helper/AppleScrollBehaviourScrollView/UIScrollViewWrapper.swift
+++ Sources/BottomSheet/Helper/AppleScrollBehaviourScrollView/UIScrollViewWrapper.swift
@@ -41,13 +41,13 @@ internal struct UIScrollViewWrapper<Content: View>: UIViewControllerRepresentabl
         viewController.scrollView.contentSize = contentSize
         
         // Align content view
-        NSLayoutConstraint.activate([
-            viewController.hostingController.view.leadingAnchor.constraint(equalTo: viewController.scrollView.leadingAnchor),
-            viewController.hostingController.view.trailingAnchor.constraint(equalTo: viewController.scrollView.trailingAnchor),
-            viewController.hostingController.view.topAnchor.constraint(equalTo: viewController.scrollView.topAnchor),
-            viewController.hostingController.view.bottomAnchor.constraint(equalTo: viewController.scrollView.bottomAnchor)
-        ])
-        viewController.view.setNeedsUpdateConstraints()
+//        NSLayoutConstraint.activate([
+//            viewController.hostingController.view.leadingAnchor.constraint(equalTo: viewController.scrollView.leadingAnchor),
+//            viewController.hostingController.view.trailingAnchor.constraint(equalTo: viewController.scrollView.trailingAnchor),
+//            viewController.hostingController.view.topAnchor.constraint(equalTo: viewController.scrollView.topAnchor),
+//            viewController.hostingController.view.bottomAnchor.constraint(equalTo: viewController.scrollView.bottomAnchor)
+//        ])
+//        viewController.view.setNeedsUpdateConstraints()
         viewController.view.updateConstraintsIfNeeded()
         viewController.view.layoutIfNeeded()
         
