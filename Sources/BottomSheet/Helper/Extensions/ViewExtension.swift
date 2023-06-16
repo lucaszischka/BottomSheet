cebd220376857550fe8dcc35bbbaa9668f348352
Chocoford <504001583@qq.com>
2023-06-16 14:29:55 +0800

    fix "Ignored keyboard safe area on iPhone." #97

---
+++ Sources/BottomSheet/Helper/Extensions/ViewExtension.swift
@@ -0,0 +1,38 @@
+//
+//  ViewExtension.swift
+//  
+//
+//  Created by Chocoford on 2023/6/16.
+//
+
+import SwiftUI
+
+enum SafeAreaRegionsCompatible {
+    case all
+    case container
+    case keyboard
+    
+    @available(iOS 14.0, macOS 11.0, *)
+    var safeAreaRegions: SafeAreaRegions {
+        switch self {
+            case .all:
+                return .all
+            case .container:
+                return .container
+            case .keyboard:
+                return .keyboard
+        }
+    }
+}
+
+internal extension View {
+    @ViewBuilder
+    func ignoresSafeAreaCompatible(_ regions: SafeAreaRegionsCompatible = .all,
+        edges: Edge.Set = .all) -> some View {
+        if #available(iOS 14.0, macOS 11.0, *) {
+            ignoresSafeArea(regions.safeAreaRegions, edges: edges)
+        } else {
+            edgesIgnoringSafeArea(edges)
+        }
+    }
+}
