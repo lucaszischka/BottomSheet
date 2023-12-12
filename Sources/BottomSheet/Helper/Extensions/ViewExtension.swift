6998a2594c2db9f7585b879751af46ce11d6818e
Jukka Hietanen <jukka.hietanen@accelbit.com>
2023-12-12 13:22:18 +0200

    In addition to handling drag gesture changes & ending, also handle cancelled drag gesture. Fixes #148.

---
+++ Sources/BottomSheet/Helper/Extensions/ViewExtension.swift
@@ -0,0 +1,24 @@
+//
+//  File.swift
+//
+//
+//  Created by Jukka Hietanen on 12.12.2023.
+//
+
+import Foundation
+
+import SwiftUI
+import Combine
+
+internal extension View {
+    /// A backwards compatible wrapper for iOS 14 `onChange`
+    @ViewBuilder func valueChanged<T: Equatable>(value: T, onChange: @escaping (T) -> Void) -> some View {
+        if #available(iOS 14.0, *) {
+            self.onChange(of: value, perform: onChange)
+        } else {
+            self.onReceive(Just(value)) { (value) in
+                onChange(value)
+            }
+        }
+    }
+}
