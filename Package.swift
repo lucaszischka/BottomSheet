845adf1778c8392f6efc3cee7e72e70881b6bbe6
Bryan Anderson <bryan1anderson@gmail.com>
2026-07-18 03:49:12 -0600

    visionOS support (#151)
    * Update Package.swift
    
    * Update BottomSheetView+Calculations.swift
    
    * scale and break up complexity so compiler worked
    
    * Update VisualEffectView.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -8,7 +8,8 @@ let package = Package(
     platforms: [
         .iOS(.v13),
         .macCatalyst(.v13),
-        .macOS(.v10_15)
+        .macOS(.v10_15),
+        .visionOS(.v1)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
